000678e525596649d8555eaff0cd38dd613df417
Rovit Lawrence Fernandes <42912367+rovit123@users.noreply.github.com>
2022-10-03 08:39:51 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,7 @@ class Simple{
 public static void main(String args[]){
     String a;
     int b;
+    String c;
   System.out.println("Hello Java");
   System.out.println("Enter your name");
   Scanner sc=new Scanner(System.in);
@@ -11,7 +12,9 @@ public static void main(String args[]){
   System.out.println("Enter your age");
     b=sc.nextInt();
     System.out.println("Entered age is: "+b);
-    
+      System.out.println("Enter your usn");
+    c=sc.nextLine();
+    System.out.println("Entered usn is: "+c);
     
   
 }
